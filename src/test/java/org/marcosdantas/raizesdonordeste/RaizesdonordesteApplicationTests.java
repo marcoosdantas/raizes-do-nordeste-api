956012532916fc96a6fc500f4e2a5cf443a1be41
Marcos Dantas <marcoosgd@gmail.com>
2026-05-10 14:53:50 -0300

    chore: adiciona Maven Wrapper e arquivos auxiliares do projeto

---
+++ src/test/java/org/marcosdantas/raizesdonordeste/RaizesdonordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marcosdantas.raizesdonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesdonordesteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
